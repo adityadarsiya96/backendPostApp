dbebf50ececf8086e912242d7a640bc993265d37
Aditya Darsiya <adityadarsiya57@gmail.com>
2025-07-02 20:54:56 +0530

    Add user schema

--- models/user.js
+++ models/user.js
@@ -1,39 +1,17 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const cookieParser = require("cookie-parser");
-const app = express();
+const mongoose = require("mongoose")
 
-// Load environment variables
-dotenv.config();
+mongoose.connect("mongodb://127.0.0.1:27017/miniproject")
 
-const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI;
+const userSchema = new mongoose.Schema({
+    username:String,
+    name:String,
+    age:Number,
+    email:String,
+    password:String,
+    posts: [{
+        type:mongoose.Schema.Types.ObjectId,ref:"post"
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.set("view engine", "ejs");
-
-// Connect to MongoDB Atlas
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    }]
 })
-.then(() => {
-  console.log("MongoDB connected");
-})
-.catch((err) => {
-  console.error("MongoDB connection error:", err);
-});
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("App is working and MongoDB is connected.");
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`erver is running on port ${PORT}`);
-});
+module.exports = mongoose.model("user",userSchema)
